582d27f5529ca1b22d25b2220f320fd7b0b40ee0
MarcelJurtz <jurtzmarcel@gmail.com>
2018-04-29 10:59:21 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JekyllFixer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
