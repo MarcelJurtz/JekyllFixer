f92b23bfb6a4e2c5bd80df91002083de661c734e
MarcelJurtz <jurtzmarcel@gmail.com>
2018-05-31 16:06:03 +0200

    Add support for pre selected values, add exclude option and default path

--- Program.cs
+++ Program.cs
@@ -1,12 +1,82 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace JekyllFixer
 {
     class Program
     {
+        const string DEFAULT_PATH = "/home/marcel/Development/Web/blog/_site";
+
+        static string jekyllPath;
+        const int MAX_INPUTS = 3;
+        static List<string> excludes = new List<string>(){"index.html", "search.html"};
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Use default path: {DEFAULT_PATH} ? (Y/n)");
+
+            if(Console.ReadKey().Key == ConsoleKey.Enter || Console.ReadKey().Key == ConsoleKey.Y) 
+            {
+                if(Directory.Exists(DEFAULT_PATH))
+                    jekyllPath = DEFAULT_PATH;
+                else
+                    jekyllPath = RequestDirectory(MAX_INPUTS);
+            } else 
+            {
+                jekyllPath = RequestDirectory(MAX_INPUTS);
+            }
+
+            // Search for posts
+            var files = FindInvalidJekyllFiles(jekyllPath);
+            Console.WriteLine($"Found {files.Count} invalid files");
+            foreach(var file in files)
+            {
+                Console.WriteLine($"{file} contained in List? {excludes.Contains(file)}");
+                if(excludes.Contains(file))
+                    continue;
+
+                string fileName = Path.GetFileNameWithoutExtension(file);
+                Console.WriteLine($"Move {fileName} to {fileName}/index.html? (Y/n)");
+                if(Console.ReadKey().Key == ConsoleKey.Enter || Console.ReadKey().Key == ConsoleKey.Y) {
+                    // Move file to folder and rename to index.html
+                    string newFolderPath = Path.Combine(Path.GetDirectoryName(file), fileName);
+                    Directory.CreateDirectory(newFolderPath);
+                    File.Move(file, Path.Combine(newFolderPath, "index.html"));
+                    Console.WriteLine($"Moving {fileName} to {Path.Combine(newFolderPath, "index.html")}");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Done. Press any key to exit");
+            Console.Read();
+        }
+
+        private static List<string> FindInvalidJekyllFiles(string path)
+        {
+            return Directory.GetFiles(path, "*.html", SearchOption.AllDirectories)
+                .Where(x => !excludes.Contains(Path.GetFileName(x))).ToList();
+        }
+
+        private static string RequestDirectory(int maxTries)
+        {
+            int inputs = 0;
+
+            do{
+                Console.WriteLine("Enter Jekyll _site path: ");
+                jekyllPath = Console.ReadLine();
+                inputs++;
+            }
+            while(!Directory.Exists(jekyllPath) && inputs < MAX_INPUTS);
+
+            if(!Directory.Exists(jekyllPath)) {
+                Console.WriteLine("Invalid path entered. Press any key to terminate.");
+                Console.Read();
+                return null;
+            }
+
+            return jekyllPath;
         }
     }
 }
